b5deb415a06ed135e75d54c6e9909430997fce25
TeytannZ <teytannz@gmail.com>
2025-09-25 23:12:22 +0100

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,30 @@
 
+function getRandomColor() {
+  const colors = ['#ff6b6b', '#4ecdc4', '#45b7d1', '#96ceb4', '#ffeaa7', '#dda0dd', '#98d8c8'];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function formatNumber(num) {
+  if (num >= 1000000) {
+    return (num / 1000000).toFixed(1) + 'M';
+  }
+  if (num >= 1000) {
+    return (num / 1000).toFixed(1) + 'K';
+  }
+  return num.toString();
+}
+
+function generateGradient(colors, direction = '135deg') {
+  return `linear-gradient(${direction}, ${colors.join(', ')})`;
+}
+
+function createPulseAnimation(duration = '2s', values = '0.7;1;0.7') {
+  return `<animate attributeName="opacity" values="${values}" dur="${duration}" repeatCount="indefinite"/>`;
+}
+
+module.exports = {
+  getRandomColor,
+  formatNumber,
+  generateGradient,
+  createPulseAnimation
+};
